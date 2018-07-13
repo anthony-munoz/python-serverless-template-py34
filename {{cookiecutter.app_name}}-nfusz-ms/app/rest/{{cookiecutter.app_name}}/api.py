8a04239940ef923abb4b5928a64db19b1181cc0a
Anthony Muñoz Mejías <anthony.munoz@lantern.tech>
2018-07-12 23:08:10 -0600

    changeson controller

--- {{cookiecutter.app_name}}-nfusz-ms/app/rest/{{cookiecutter.app_name}}/api.py
+++ {{cookiecutter.app_name}}-nfusz-ms/app/rest/{{cookiecutter.app_name}}/api.py
@@ -8,8 +8,8 @@ import datetime
 
 from flask_restplus import Resource
 from app.rest.api import api
-from app.rest.analytics.serializers import stats_serializer
-from app.analytics.analytics import AnalyticsController
+from app.rest.{{cookiecutter.app_name}}.serializers import stats_serializer
+from app.{{cookiecutter.app_name}}.{{cookiecutter.app_name}} import {{cookiecutter.app_name}}Controller
 
 log = logging.getLogger(__name__)
 ns = api.namespace('{{cookiecutter.app_name}}', description='Get statistics users information in your notifiCRM account')
@@ -20,73 +20,6 @@ class Stats(Resource):
 
     @api.expect(stats_serializer, validate=True)
     def get(self):
-        post_mail = AnalyticsController()
+        post_mail = {{cookiecutter.app_name}}Controller()
         return post_mail.controller(**api.payload)
-
-
-@ns.route('/get_leads')
-class Stats1(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def get(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-@ns.route('/get_view_trend')
-class Stats2(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def get(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-@ns.route('/get_click_trend')
-class Stats3(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def get(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-@ns.route('/create_predictive_model')
-class Stats4(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def post(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-@ns.route('/create_descriptive_model')
-class Stats5(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def post(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-@ns.route('/create_regression_model')
-class Stats6(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def post(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-
-@ns.route('/create_report')
-class Stats7(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def post(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
-@ns.route('/create_summary')
-class Stats8(Resource):
-
-    @api.expect(stats_serializer, validate=True)
-    def post(self):
-        post_mail = AnalyticsController()
-        return post_mail.controller(**api.payload)
-
 
--- {{cookiecutter.app_name}}-nfusz-ms/app/{{cookiecutter.app_name}}/{{cookiecutter.app_name}}.py
+++ {{cookiecutter.app_name}}-nfusz-ms/app/{{cookiecutter.app_name}}/{{cookiecutter.app_name}}.py
@@ -8,12 +8,12 @@ from app.flask_factory import settings
 log = logging.getLogger(__name__)
 
 
-class AnalyticsController:
+class {{cookiecutter.app_name}}Controller:
 
 	def __init__(self):
-		self.type = "analytics"
+		self.type = "{{cookiecutter.app_name}}"
 
 	def controller(self, user):
-		response = 'User information for Kurt'
+		response = 'User information for {{cookiecutter.app_name}}'
 
 		return response
